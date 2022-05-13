ddf3afa0a3d6ddc8ac938a130e06a76b231c4512
Jake Bartolin <nixpardus+github@pm.me>
2022-05-13 02:57:08 -0400

    Removed data output to the .txt file.

--- workout_data_cleanup.py
+++ workout_data_cleanup.py
@@ -16,39 +16,42 @@
 import re
 import sys
 
+def ProcessData(data):
+
+    isFirstExcercise = True
+    output = ""
+
+    for line in data:
+
+        line = re.sub('\n', '', line)       # Remove newline characters
+
+
+        if (line == ''):                    # If line is empty, skip it
+            continue
+        elif (line.startswith("N:0")):      # If line is empty note, mark as "N/A"
+            line = 'xxN/A'
+        elif (line.startswith("E:")):       # If line is excercise, add first pipe
+            line = 'xx|' + line[2:]
+            if (isFirstExcercise == True):  # If not first excercise, add newline
+                isFirstExcercise = False
+            else:
+                line = 'xx\n' + line[2:]
+        elif (line.startswith("N:")):       # If line is note, replace line breaks with actual breaks
+            line = re.sub('tt- ', '<br>- ', line)
+
+        output += line[2:]                  # Add line minus the first two characters to Output
+
+        if (output.endswith('|') == False):
+            output += '|'                   # If no pipe at end of Output, add pipe
+    return output
+
 readfile = open("paste_workout_data_here.txt", "r")
 
 Input = []
 Output = ""
-isFirstExcercise = True
 
 Input = readfile.readlines()
+Output = ProcessData(Input)
 
-for line in Input:
-
-    line = re.sub('\n', '', line)       # Remove newline characters
-
-
-    if (line == ''):                    # If line is empty, skip it
-        continue
-    elif (line.startswith("N:0")):      # If line is empty note, mark as "N/A"
-        line = 'xxN/A'
-    elif (line.startswith("E:")):       # If line is excercise, add first pipe
-        line = 'xx|' + line[2:]
-        if (isFirstExcercise == True):  # If not first excercise, add newline
-            isFirstExcercise = False
-        else:
-            line = 'xx\n' + line[2:]
-    elif (line.startswith("N:")):       # If line is note, replace line breaks with actual breaks
-        line = re.sub('tt- ', '<br>- ', line)
-
-    Output += line[2:]                  # Add line minus the first two characters to Output
-
-    if (Output.endswith('|') == False):
-        Output += '|'                   # If no pipe at end of Output, add pipe
-
-# Write the Output to the File
-writefile = open("cleaned_data.txt", "w")
-writefile.write("|Excercise|Sets/Reps|Weight|Reserve Reps|Notes|\n|---|---|---|---|---|\n")
-writefile.write(Output)
-writefile.close()
+print("|Excercise|Sets/Reps|Weight|Reserve Reps|Notes|\n|---|---|---|---|---|")
+print(Output)
