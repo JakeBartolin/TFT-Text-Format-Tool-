910f782d15c53409f807ec4e3c5247081536e809
Jake Bartolin <nixpardus+github@pm.me>
2022-07-06 14:51:27 -0400

    project not a general text format tool (TFT). Add Main() and menu loop. Added chord chart formatting and started lyrics extraction.

--- workout_data_cleanup.py
+++ workout_data_cleanup.py
@@ -1,8 +1,46 @@
 import re
 import sys
 
-def GetData():
-    print("\n\nPlease paste your data below using Ctrl+Shift+V.\nWhen you are done, press Ctrl+D for Linux/Mac or Ctrl+Z for Windows.\n\n")
+def Main():
+    userInput = ""
+    isActive = True
+    welcomeMessage = """Starting TFT Script...
+
+********************
+* Text Format Tool *
+********************
+
+"""
+    functionSelect = """Please select a function:
+1) Format Workout Data
+2) Format Chord Charts
+3) Extract Lyrics
+x) Exit
+
+"""
+
+    print(welcomeMessage)
+
+    while isActive == True:
+        print(functionSelect)
+        userInput = input()
+
+        if userInput == "1":
+            FormatWorkoutData()
+        elif userInput == "2":
+            FormatChordCharts()
+        elif userInput == "3":
+            ExtractLyrics()
+        elif userInput == "x":
+            isActive = False
+        else:
+            print("Sorry, not a valid entry, please try again.\n\n")
+
+    print("Exiting Script....")
+
+def GetText():
+    
+    print("Please paste your data below using Ctrl+Shift+V.\nWhen you are done, press Ctrl+D for Linux/Mac or Ctrl+Z for Windows.\n")
     lines = []
     try:
         lines = sys.stdin.readlines()
@@ -10,7 +48,44 @@ def GetData():
         pass
     return lines
 
-def ProcessData(data):
+def FormatWorkoutData():
+    print("Starting Format Workout Data subscript...\n")
+    text = GetText()
+    text = ProcessWorkoutData(text)
+    PrintWorkoutData(text)
+    return
+
+def FormatChordCharts():
+    text = GetText()
+    output = ""
+
+    for line in text:
+        if line == "\n":
+            continue
+        else:
+            output = output + line
+
+    print("Your chord chart is printed below:\n******************************\n\n\n")
+    print(output)
+    return
+
+def ExtractLyrics():
+    text = GetText()
+    output = ""
+
+    for line in text:
+        if line == "\n":
+            continue
+
+        if line == "[Chorus]":
+            line = "# Chorus"
+        
+        output += line
+    
+    print(text)
+    return
+
+def ProcessWorkoutData(data):
 
     isFirstExcercise = True
     output = ""
@@ -38,14 +113,12 @@ def ProcessData(data):
             output += '|'
     return output
 
-def PrintData(data):
+def PrintWorkoutData(data):
     # Many print() calls are much easier to look at in the code
     # and I'm not too worried about the speed of a 50 line text formatting script.
-    print("\n\nYour formatted data is printed below:\n*************************************\n")
+    print("\n\nYour formatted data is printed below:\n\n")
     print("|Excercise|Sets/Reps|Weight|Reserve Reps|Notes|\n|---|---|---|---|---|")
     print(data)
-    print("\n*************************************\n")
+    print("\n\n")
 
-data = GetData()
-data = ProcessData(data)
-PrintData(data)
+Main()
